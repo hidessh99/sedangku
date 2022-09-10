716254899eaa89ed7ec92f153081cb6d5ff7295a
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-09-10 18:03:44 +0700

    Update menu-backup.sh

--- update/menu-backup.sh
+++ update/menu-backup.sh
@@ -79,7 +79,7 @@ echo -e " $COLOR1│$NC                                               $COLOR1│
 echo -e " $COLOR1│$NC   ${COLOR1}[00]${NC} • GO BACK${NC}                              $COLOR1│$NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                • WWW.SSHSEDANG.MY.ID •             $COLOR1│$NC"
+echo -e "$COLOR1│${NC}                • WWW.SSHSEDANG.MY.ID •          $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -p " Select menu :  "  opt

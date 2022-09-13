89206dbecc511b5d4bab6faf80ede920f1abb1c8
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-09-13 11:14:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -305,7 +305,7 @@ echo "------------------------------------------------------------"
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - SSH Websocket           : 80 [OFF], 8880" | tee -a log-install.txt
+echo "   - SSH Websocket           : 80 [Turn It On], 8880" | tee -a log-install.txt
 echo "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
 echo "   - Stunnel4                : 447, 777" | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143" | tee -a log-install.txt

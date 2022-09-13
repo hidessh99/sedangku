11bd3051257dc4588c16c75589d0c05c8714183f
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-09-13 10:47:22 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -16,7 +16,6 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/ascii-home | lolcat
 echo "SSH Websocket" | lolcat
 echo "Websocket TLS / Websocket None TLS" | lolcat
 echo "Progress..." | lolcat

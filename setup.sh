4b1cf8c09c12ac127804f97732320c83c2c079fb
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-09-13 13:21:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -154,7 +154,7 @@ chmod 644 /root/.profile
 
 echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
+echo -e "[ ${green}INFO${NC} ] Allright good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 

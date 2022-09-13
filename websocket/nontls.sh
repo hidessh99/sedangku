b165c8510529129a8b7bb5221e3edf94f65a56e1
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-09-13 14:59:16 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,8 +16,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "SSH Websocket" | lolcat
-echo "Websocket None TLS" | lolcat
+echo "Installing SSH Websocket None TLS" | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
